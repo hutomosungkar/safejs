0738e30ce2a89989a91292a89277d80950babec1
Hutomo Sungkar <69704688+hutomosungkar@users.noreply.github.com>
2020-09-06 19:48:15 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="geico-auto-insurance-retrieve-quote.html"}
 if(x=="2"){xxx="how-to-get-free-insurance-geico.html"}
 if(x=="3"){xxx="auto-insurance-quotes-charlotte-nc.html"}
-if(x=="4"){xxx="geico-motorcycle-insurance-quote-online.html"}a_to_ve[i].href="https://safelinkpest.blogspot.com/2020/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="geico-motorcycle-insurance-quote-online.html"}a_to_ve[i].href="https://safe.pestcarejakarta.com/2020/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
